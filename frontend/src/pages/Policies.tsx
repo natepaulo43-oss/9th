7e25e8666025600392e95d17c1c9af9b1ee1d560
Nate <natepaulo43@gmail.com>
2026-03-31 14:58:58 -0400

    Update shipping policy from free shipping to flat rate $3.99 on all orders

--- frontend/src/pages/Policies.tsx
+++ frontend/src/pages/Policies.tsx
@@ -21,7 +21,7 @@ const sections: {
       'Pre-Orders will all be shipped out on the dropdate.',
     ],
     highlights: [
-      'Free Shipping. All the time.',
+      'Flat rate shipping of $3.99 on all orders.',
       'Customers are responsible for any import duties or taxes assessed by their country.',
     ],
   },
